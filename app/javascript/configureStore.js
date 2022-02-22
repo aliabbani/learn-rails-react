a18fac628a451bf9b696ed8f6e9c182fee355f16
ali abbani <aliox@ahlam.com>
2022-02-22 14:36:44 +0200

    display api in the app using react rails

--- app/javascript/configureStore.js
+++ app/javascript/configureStore.js
@@ -1,18 +1,30 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+
+import thunk from "redux-thunk";
 
 const initialState = {
-    things: []
+    things: [
+        {
+            name: 'test',
+            guid: '123'
+        }
+    ]
 };
 
 function rootReducer(state, action) {
     console.log(action.type);
     switch (action.type) {
-        default:
-            return state
+      case "GET_THINGS_SUCCESS":
+        return { things: action.json.things };
     }
+    return state;
 }
 
 export default function configureStore() {
-    const store = createStore(rootReducer, initialState);
+    const store = createStore(
+        rootReducer,
+        initialState,
+        applyMiddleware(thunk)
+    );
     return store;
 }
