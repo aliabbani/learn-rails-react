a7191fc048f7dac056d24aa91c71c5f8fd76b127
ali abbani <aliox@ahlam.com>
2022-02-22 13:18:16 +0200

    Display a button and make sure the action is working well

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -3,19 +3,26 @@ import PropTypes from "prop-types"
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import { Provider } from "react-redux"
+
 import HelloWorld from './HelloWorld'
 import User from "./User"
+
+import configureStore from "../configureStore"
+const store = configureStore();
 
 class App extends React.Component {
   render () {
     return (
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<HelloWorld greeting={'Henry'} />} />
-          <Route path='/hello' element={<HelloWorld greeting={'Ali'} />} />
-          <Route path='/user' element={<User />} />
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<HelloWorld greeting={'Henry'} />} />
+            <Route path='/hello' element={<HelloWorld greeting={'Ali'} />} />
+            <Route path='/user' element={<User />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
